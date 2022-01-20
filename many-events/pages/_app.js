255972bf0b5923808a601487b873a74427d58148
Ben MartinMooney <ben.martinmooney@gmail.com>
2022-01-19 22:29:19 -0500

    style branch prep

--- many-events/pages/_app.js
+++ many-events/pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css';
+import Layout from "../components/layout/Layout"
 
 function EventsApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return <Layout><Component {...pageProps} /></Layout>;
 }
 
 export default EventsApp;
